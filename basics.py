a1671fdee9cce8053c249475263e6c799fade146
Farooq Butt <farooqbutt548@gmail.com>
2021-09-23 12:26:13 +0500

    Basics

---
+++ basics.py
@@ -0,0 +1,35 @@
+'''# ----String
+numbers = (1,2,3)
+name1 = ['abc', 'bcd','cde', 'def']
+
+# ----numbers
+integer = [1,2,3,4,5]
+floate = [1.2, 2.33,4.0]
+complexe = (10+2-j)
+esj'''
+# input("\n\nPress the enter key to exit."); input("\n\nPress the enter key to exit."); input("\n\nPress the enter key to exit.")
+#<button></button> successfully
+# ----operators
+    # -- Addition
+'''a = 5 ; b = 11
+print(a+b)
+    # --Subtraction
+print(b-a)
+    # --Multiplication
+print(a*b)
+    # --Division
+print(b/a); print(b//a)
+    # --Modulus
+print(b%a) 
+    # --Exponent(power)
+print(2**3)
+
+print(a!=b)
+
+    # ---in
+name =[1,2,3,4,5,6]
+if 2 in name:
+    print('it\'s is present')
+if 7 not in name:
+    #123
+    print("it's true statement") '''
