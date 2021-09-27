ac5ef315b84ebf9b3f1ea21ee467338b7f4d33d2
Farooq Butt <farooqbutt548@gmail.com>
2021-09-27 19:58:16 +0500

    Classes/Objects

--- classesObject.py
+++ classesObject.py
@@ -54,12 +54,28 @@ print(Laptop.count_instance)
         self.subject = subject
         self.shift = shift
         self.cgpa = cgpa
-        self.fee = 40000
+
+        if subject == 'BS-IT':                  # we use if else in attribute
+            self.fee =35000
+        elif subject == 'BS-SE':
+            self.fee = 40000
+        elif subject == 'BS-CS':
+            self.fee = 30000
+        else:
+            self.fee = 'enter correct Subject'
+
 
 farooq = University( input('enter department : '), input('enter subject : '), input('enter Shift : '), input('enter cgpa : ' ))
-print(f'Your, department : {farooq.department} , subject : {farooq.subject} , shift : {farooq.shift} , cgpa : {farooq.cgpa} , fee : {farooq.fee}')
-print(farooq.total_marks)       # class's variable/attribute, object derieved from class (Inheretance)
-print(University.total_marks)       # class's variable/attribute'''
+
+# print(f'Your, department : {farooq.department} , subject : {farooq.subject} , shift : {farooq.shift} , cgpa : {farooq.cgpa} , fee : {farooq.fee}')
+
+print(f'Your Subject is "{farooq.subject}"')
+print(f'Your Shift is "{farooq.shift}"')
+print(f'Your CGPA is "{farooq.cgpa}"')
+print(f'Your Fee is "{farooq.fee}"')
+
+# print(farooq.total_marks)       # class's variable/attribute, object derieved from class (Inheretance)
+# print(University.total_marks)       # class's variable/attribute'''
 
 
 
@@ -91,3 +107,31 @@ print(f'Hi Dear your CGPA : {touseef.cgpa} .')
 
 print(University.intro())       # this is static method (instance/class independent)
 print(touseef.intro())       # this is static method (instance/class independent)'''
+
+
+
+'''class Phone:
+    def __init__(self, name, model, price):
+        self.name = name
+        self._model = model
+        self.__price = max(price, 0)    # if price negative it will assume zero 
+
+ph1 = Phone('techno', 'spark_5', 18000)
+print(ph1.name)
+print(ph1._model)
+# print(ph1.__price)  # mangling name error 
+print(ph1._Phone__price)        # in case of mangling we concatinate _ClassName then mangling attribute
+ph1._Phone__price =-18500
+print(ph1._Phone__price)    # in mangling Python changes it's name 
+
+
+class latest_phone(Phone):              # inheritance
+    def __init__(self, name, model, price, pixel, size):
+        super().__init__(name, model, price)
+        self.pixel = pixel
+        self.size = size
+
+ph2 = latest_phone('infinix', 'camon 11', 20000, '8 mpx', '8 inch' )
+
+print(f"the old phone's properties is {ph1.name}, {ph1._model}, {ph1._Phone__price} ")
+print(f"the latest phone's properties is Name: {ph2.name},Model: {ph2._model},Price: {ph2._Phone__price}, Pixel: {ph2.pixel}, Size: {ph2.size} ")'''
